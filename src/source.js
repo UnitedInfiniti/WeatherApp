18887c82215adda2578ad5f5fe92e14b172de703
Kyla Loasby <119205843+UnitedInfiniti@users.noreply.github.com>
2023-02-12 13:02:45 -0500

    updates js code for additional weather detail elements

--- src/source.js
+++ src/source.js
@@ -55,14 +55,19 @@ h3.innerHTML = formatDate() + " | " + formatTime();
 
 //City Search
 function changeTemperature(response) {
-  let currentTemperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#current-temperature");
-  temperatureElement.innerHTML = `${currentTemperature}`;
-  let currentConditions = response.data.weather[0].description;
-  let conditionsElement = document.querySelector("#condition");
-  conditionsElement.innerHTML = `${currentConditions}`;
-  let city = document.querySelector("#city-name");
-  city.innerHTML = response.data.name;
+  let currentCityElement = document.querySelector("#city-name");
+  let currentCity = response.data.name;
+  currentCityElement.innerHTML = `${currentCity}`;
+  let currentTempElement = document.querySelector("#current-temperature");
+  let currentTemp = Math.round(response.data.main.temp);
+  currentTempElement.innerHTML = `${currentTemp} °`;
+  let currentConditionsElement = document.querySelector("#condition");
+  let currConditions = response.data.weather[0].description;
+  currentConditionsElement.innerHTML = `${currConditions}`;
+  let lowTemp = Math.round(response.data.main.temp_min);
+  let highTemp = Math.round(response.data.main.temp_max);
+  let highlowElement = document.querySelector("#high-low");
+  highlowElement.innerHTML = `${lowTemp}°  |  ${highTemp}°`;
 }
 
 function changeCity(event) {
@@ -78,10 +83,26 @@ function changeToCurrentLocation(position) {
   currentCityElement.innerHTML = `${currentCity}`;
   let currentTempElement = document.querySelector("#current-temperature");
   let currentTemp = Math.round(position.data.main.temp);
-  currentTempElement.innerHTML = `${currentTemp}`;
+  currentTempElement.innerHTML = `${currentTemp} °`;
   let currentConditionsElement = document.querySelector("#condition");
   let currConditions = position.data.weather[0].description;
   currentConditionsElement.innerHTML = `${currConditions}`;
+  let currentfeelElement = document.querySelector("#feelsLike");
+  let currentFeels = Math.round(position.data.main.feels_like);
+  currentfeelElement.innerHTML = `Feels Like: ${currentFeels} °`;
+  let lowTemp = Math.round(position.data.main.temp_min);
+  let highTemp = Math.round(position.data.main.temp_max);
+  let highlowElement = document.querySelector("#high-low");
+  highlowElement.innerHTML = `${lowTemp}°  |  ${highTemp}°`;
+  let currentwindElement = document.querySelector("#wind");
+  let currentWind = Math.round(position.data.wind.speed);
+  currentwindElement.innerHTML = `${currentWind} km/h`;
+  let currenthumidElement = document.querySelector("#humidity");
+  let currentHumid = position.data.main.humidity;
+  currenthumidElement.innerHTML = `${currentHumid} %`;
+  let currentvisElement = document.querySelector("#visibility");
+  let currentVis = position.data.visibility;
+  currentvisElement.innerHTML = `${currentVis}`;
   console.log(position);
 }
 function receiveCurrentPosition(position) {
